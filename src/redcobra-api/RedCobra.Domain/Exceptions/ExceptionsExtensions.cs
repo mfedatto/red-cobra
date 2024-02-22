1dc79d0fba5d9d3df93b674cc974f83341abd39b
Mauricio Fedatto <mfedatto@gmail.com>
2024-02-22 06:05:24 -0300

    request closed exception;

---
+++ src/redcobra-api/RedCobra.Domain/Exceptions/ExceptionsExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace RedCobra.Domain.Exceptions;
+
+public static class ExceptionsExtensions
+{
+    public static void ThrowIfClientClosedRequest(this CancellationToken cancellationToken)
+    {
+        if (cancellationToken.IsCancellationRequested) throw new RequestClosedException();
+    }
+}
---
+++ src/redcobra-api/RedCobra.Domain/Exceptions/RequestClosedException.cs
@@ -0,0 +1,12 @@
+﻿using RedCobra.HttpExceptions;
+
+namespace RedCobra.Domain.Exceptions;
+
+public class RequestClosedException : Http499ClientClosedRequestException
+{
+    private const string HttpExceptionMessage = "Request closed.";
+
+    public RequestClosedException() : base(HttpExceptionMessage) { }
+    
+    public RequestClosedException(Exception innerException) : base(HttpExceptionMessage, innerException) { }
+}
