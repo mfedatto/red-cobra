b5347e257fc3c355432ec576e984366cdfe2737d
Mauricio Fedatto <mfedatto@gmail.com>
2024-02-23 11:12:25 -0300

    cypress api requests;

--- e2e/cypress/e2e/red-cobra-api.cy.js
+++ e2e/cypress/e2e/red-cobra-api.cy.js
@@ -1,7 +1,8 @@
 import users from '../fixtures/a1tier-users.json';
 import '../support/functions.js';
 
-const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const uuidPattern =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 describe('Tier A1 - Default Dataset: /v1/users', () => {
   users.forEach((user) => {
@@ -10,18 +11,10 @@ describe('Tier A1 - Default Dataset: /v1/users', () => {
     it(`Gets a user (${user.username})`, () => {
       cy.request({
         method: 'GET',
-        url: `/v1/users?username=${user.username}`
+        url: `/v1/users?username=${user.username}`,
       }).then((response) => {
         expect(response.status).to.eq(200);
-        expect(response.body).to.be.an('object');
-        expect(response.body.total).to.not.be.eq(null);
-        expect(response.body.total).to.be.an('number');
-        expect(response.body.skip).to.not.be.eq(null);
-        expect(response.body.skip).to.be.an('number');
-        expect(response.body.limit).to.not.be.eq(null);
-        expect(response.body.limit).to.be.an('number');
-        expect(response.body.payload).to.not.be.eq(null);
-        expect(response.body.payload).to.be.an('array');
+        cy.redCobra.pagedListWrapperAssertions(response.body);
         expect(response.body.payload.length).to.be.lt(2);
 
         if (response.body.payload.length > 0) {
@@ -36,9 +29,9 @@ describe('Tier A1 - Default Dataset: /v1/users', () => {
       cy.request({
         method: 'DELETE',
         url: `/v1/users/${userIdToDelete}`,
-        failOnStatusCode: false
+        failOnStatusCode: false,
       }).then((response) => {
-        expect([ 200, 404, 409 ]).to.contain(response.status);
+        expect([200, 404, 409]).to.contain(response.status);
       });
     });
     it(`Creates a user (${user.username})`, () => {
@@ -54,23 +47,23 @@ describe('Tier A1 - Default Dataset: /v1/users', () => {
         },
       }).then((response) => {
         expect(response.status).to.eq(200);
-        cy.redCobra.userAssertion(response.body, user);
+        cy.redCobra.userAssertions(response.body, user);
       });
     });
   });
+  it('Gets a list of users', () => {
+    cy.request({
+      method: 'GET',
+      url: '/v1/users',
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      cy.redCobra.pagedListWrapperAssertions(response.body);
 
-//  it('Gets a list of users', () => {
-//    cy.request({
-//      method: 'GET',
-//      url: '/v1/users',
-//    }).then((response) => {
-//      expect(response.status).to.eq(200);
-//      expect(response.body).to.be.an('object');
-//      expect(response.body.total).to.not.be.eq(null);
-//      expect(response.body.skip).to.not.be.eq(null);
-//      expect(response.body.limit).to.not.be.eq(null);
-//      expect(response.body.payload).to.not.be.eq(null);
-//      expect(response.body.payload).to.be.an('array');
-//    });
-//  });
+      if (response.body.payload.length > 0) {
+        response.body.payload.forEach((user) => {
+          cy.redCobra.userAssertions(user);
+        });
+      }
+    });
+  });
 });
